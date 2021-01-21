fff0df85bbfe768e5541f5d16ded3fd7375f9bff
wssh <wesley.hinsch@matera.com>
2021-01-21 12:46:16 -0300

    First commit and change port application.

---
+++ src/main/java/com/sicred/api/schedule/ApiScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.sicred.api.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sicred/api/schedule/ApiScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicred.api.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
